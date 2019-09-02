40e8d49d47b2f58a2e8dfe97f602ee708253fa47
GeorgeNa <georgena@stlghana.com>
2019-09-02 18:07:25 +0000

    Implemented security configuration

--- src/main/java/com/nanor/springldap/SpringLdapApplication.java
+++ src/main/java/com/nanor/springldap/SpringLdapApplication.java
@@ -2,6 +2,16 @@ package com.nanor.springldap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
 public class SpringLdapApplication {
@@ -9,5 +19,42 @@ public class SpringLdapApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringLdapApplication.class, args);
     }
+}
 
+@RestController
+class GreetingRestController {
+
+    @GetMapping(value = {"/greeting", "/"})
+    String greet(Principal principal) {
+        return "hello " + principal.getName();
+    }
+}
+
+@Configuration
+@EnableWebSecurity
+class LdapSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        //@formatter:off
+        authenticationManagerBuilder
+                .ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=springframework,dc=org") // use ldap server here
+                .and()
+                .passwordCompare()
+                .passwordEncoder(new LdapShaPasswordEncoder())
+                .passwordAttribute("userPassword");
+        //@formatter:on
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .formLogin();
+    }
 }
